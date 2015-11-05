7532a9584c5b2522849e65b49cbd13d6cc69aa0a
Nicole <nicoledevillers@gmail.com>
2015-11-04 19:39:58 -0800

    style modal, allow replaying, some refactoring

--- modal.js
+++ modal.js
@@ -1,18 +1,30 @@
-$("#modal").modal({
- escapeClose: false,
- clickClose: false,
- showClose: false,
- opacity: 0
-});
-
-
 /*jshint multistr: true */
+
+var modalOptions = {
+  escapeClose: false,
+  clickClose: false,
+  showClose: false,
+  opacity: 0
+};
 
 var startModalContent = '<h1>Welcome to Asteroids!</h1> \
 <ul> \
   <li> Use left and right arrow keys to rotate </li> \
-  <li> Power your ship by pressing up.  You can go backwards by pressing down.</li> \
+  <li> Power your ship by pressing up  You can go backwards by pressing down.</li> \
+  <li> Go backwards by pressing down </li> \
   <li> Fire bullets using the spacebar </li> \
   <li> Have fun! \
 </ul> \
-<button type="button" name="button" onclick="startGame()" rel="modal:close">start!</button>';
+<button type="button" name="button" onclick="Asteroids.startGame()" rel="modal:close">start!</button>';
+
+var endModalContent = function(highScore, score) {
+  return '<h1>Game Over</h1>' +
+  '<p>your score: ' +
+  score +
+  '</p> <p>high score: ' +
+  highScore +
+  '</p>' +
+  restartButton;
+};
+
+var restartButton = '<button class="asteroids-button" onclick="Asteroids.restartGame()">play again!</button>';
